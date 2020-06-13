9a7068d0976db52f0976c72f406e5990d24c3111
Joel Ruhland <joel@joelruhland.net>
2020-06-13 19:46:40 -0400

    Fixed failing test, added additional testing+benchmarks

--- jdc_test.go
+++ jdc_test.go
@@ -1,10 +1,12 @@
-package JDC
+package jdc
 
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestKeyDer(t *testing.T) {
@@ -31,33 +33,59 @@ func TestIVGen(t *testing.T) {
 func TestPadding(t *testing.T) {
 	iv := generateIV()
 
-	data := []byte{5,5,5,5,5,5}
+	data := []byte{}
 	newData := padData(data, iv)
-	if len(newData) != BLOCK_LEN { t.Fail() }
+	if len(newData) != blockLen {
+		t.Errorf("Incorrect padded length for no data")
+	}
 
-	data = []byte{5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5}
+	data = make([]byte, 5)
+	for i := 0; i < 5; i++ {
+		data[i] = 5
+	}
 	newData = padData(data, iv)
-	if len(newData) != 2*BLOCK_LEN { t.Fail() }
+	if len(newData) != 2*blockLen {
+		t.Errorf("Incorrect padded length for 5 bytes data")
+	}
 
-	data = []byte{5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5}
+	data = make([]byte, 32)
+	for i := 0; i < 32; i++ {
+		data[i] = 5
+	}
 	newData = padData(data, iv)
-	if len(newData) != 2*BLOCK_LEN { t.Fail() }
+	if len(newData) != 2*blockLen {
+		t.Errorf("Incorrect padded length for 32 bytes data")
+	}
+
+	data = append(data, 5)
+	newData = padData(data, iv)
+	if len(newData) != 3*blockLen {
+		t.Errorf("Incorrect padded length for 33 bytes data")
+	}
 }
 
 func TestEncryptDecryptBasic(t *testing.T) {
-	input := []byte{5,5,5,5}
+	input := []byte{5, 5, 5, 5}
 	data := Encrypt(input, "hello")
-	fmt.Println(data)
 	data = Decrypt(data, "hello")
-	fmt.Println(data)
 
-	if !reflect.DeepEqual(data,input) {
+	if !reflect.DeepEqual(data, input) {
+		t.Fail()
+	}
+
+	input = make([]byte, 1e3)
+	rand.Read(input)
+
+	data = Encrypt(input, "password123")
+	data = Decrypt(data, "password123")
+
+	if !reflect.DeepEqual(data, input) {
 		t.Fail()
 	}
 }
 
 func TestLongEncryptDecrypt(t *testing.T) {
-	data,_ := ioutil.ReadFile("test_input.txt")
+	data, _ := ioutil.ReadFile("test_input.txt")
 
 	data = Encrypt(data, "password123")
 	data = Decrypt(data, "password123")
@@ -66,16 +94,59 @@ func TestLongEncryptDecrypt(t *testing.T) {
 	if !reflect.DeepEqual(data, originalData) {
 		t.Fail()
 	}
+
+	input := make([]byte, 1e6)
+	rand.Read(input)
+
+	data = Encrypt(input, "password123")
+	data = Decrypt(data, "password123")
+
+	if !reflect.DeepEqual(data, input) {
+		t.Fail()
+	}
 }
 
 func TestWrongKey(t *testing.T) {
-	data,_ := ioutil.ReadFile("test_input.txt")
+	data, _ := ioutil.ReadFile("test_input.txt")
 
 	data = Encrypt(data, "password123")
 	data = Decrypt(data, "password")
 
 	originalData, _ := ioutil.ReadFile("test_input.txt")
 	if reflect.DeepEqual(data, originalData) {
-		t.Fail()
+		t.Errorf("Decryption with wrong key did not fail")
+	}
+
+	data, _ = ioutil.ReadFile("test_input.txt")
+	data = Encrypt(data, "password123")
+	data = Decrypt(data, "password1")
+
+	if data != nil {
+		t.Errorf("Decryption with wrong key did not fail correctly")
+	}
+}
+
+// TODO benchmark for large amoutn of data encryption
+func BenchmarkEncrypt(b *testing.B) {
+	data := make([]byte, 1e7, 1e7) //10 mb of data for testing
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(data)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Encrypt(data, "password")
+	}
+}
+
+func BenchmarkDecrypt(b *testing.B) {
+	data := make([]byte, 1e7, 1e7) //10 mb of data for testing
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(data)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Decrypt(data, "password")
 	}
 }
